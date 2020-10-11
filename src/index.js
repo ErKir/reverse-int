bb7ae0ff2df463dbf9e61aad1af22ca57be88902
ErKir <kirillerohin@gmail.com>
2020-10-11 20:51:21 +0300

    feat: implement function that reverse digits

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
-}
+    return Math.abs(n).toString()
+    .replace(/0*$/,"")
+    .split('')
+    .reverse().join('');
+ };
+
+
+    // const abs = Math.abs(n);
+    // const str = abs.toString();
+    // const replace = str.replace(/0*$/,"");
+    // const split = replace.split('');
+    // const reverse = split.reverse().join('');
+    // return Number(reverse);
+ 
